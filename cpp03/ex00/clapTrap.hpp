371019d37edc7046d7f75e167013d9e4fd5db823
Mohammed Elachyry <melachyr@c3r9p9.1337.ma>
2024-08-25 23:54:59 +0100

    add ex03 in cpp00

---
+++ cpp03/ex00/clapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melachyr <melachyr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 22:56:58 by melachyr          #+#    #+#             */
+/*   Updated: 2024/08/25 23:48:21 by melachyr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class	ClapTrap
+{
+	private:
+		std::string		name;
+		unsigned int	hitPoints;
+		unsigned int	energyPoints;
+		unsigned int	attackDamage;
+	
+	public:
+		ClapTrap( void );
+		ClapTrap( std::string name );
+		ClapTrap( const ClapTrap& clapTrap);
+		~ClapTrap( void );
+		ClapTrap&	operator = (const ClapTrap& clapTrap);
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melachyr <melachyr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 22:59:06 by melachyr          #+#    #+#             */
+/*   Updated: 2024/08/25 23:47:35 by melachyr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "clapTrap.hpp"
+
+int	main( void )
+{
+	ClapTrap clapTrap1("boo");
+
+	clapTrap1.attack("foo");
+	clapTrap1.takeDamage(5);
+	clapTrap1.beRepaired(3);
+	clapTrap1.takeDamage(8);
+	clapTrap1.attack("Too");
+
+	return (0);
+}
